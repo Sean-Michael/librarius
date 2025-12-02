d683dbbbb86d59e8280256af6700414748a13d14
Sean-Michael <seanm.riesterer@gmail.com>
2025-12-01 19:02:40 -0800

    cleanup for process thread team version

--- lexicanium.py
+++ lexicanium.py
@@ -13,23 +13,6 @@ import shutil
 zip_file_dir = "./archive"
 destination_directory = "./Data-Slates"
 
-def load_from_archive(zip_paths):
-
-    zip_files = [zipfile.ZipFile(filename, 'r') for filename in zip_paths]
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_zip = {executor.submit(zf.extractall, destination_directory): zf for zf in zip_files}
-        for future in concurrent.futures.as_completed(future_to_zip):
-            data = future_to_zip[future]
-            try:
-                future.result()
-                print(f'Extracted: {data}')
-            except Exception as e:
-                print(f'ERROR: {e}')
-            future_to_zip[future].close()
-
-    return os.listdir(destination_directory)
-
 
 def extract_zip(filepath):
     try:
@@ -60,15 +43,6 @@ def main():
         exit(1)
 
     zip_paths = [os.path.join(zip_file_dir, filepath) for filepath in os.listdir(zip_file_dir)]
-
-
-    start_thread_time = time.perf_counter()
-    load_from_archive(zip_paths)
-    end_thread_time = time.perf_counter()
-    elapsed_thread_time = end_thread_time - start_thread_time
-    print(f'Thread team finished in {elapsed_thread_time}')
-   
-    shutil.rmtree(os.path.join(destination_directory))
 
     start_proc_time = time.perf_counter()
     proc_load_from_archive(zip_paths)
